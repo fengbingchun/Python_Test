5127f7309081d4c6c1d107b54e05bf79df77d741
fengbingchun <fengbingchun@163.com>
2022-06-12 10:11:34 +0800

    Add python pass test code

---
+++ demo/simple_code/test_pass.py
@@ -0,0 +1,34 @@
+﻿# Blog: https://blog.csdn.net/fengbingchun/article/details/125242357
+
+# 1. empty function
+def func():
+	pass # remember to implement this
+
+func()
+
+# 2. empty class
+class fbc:
+	pass
+
+fbc()
+
+# 3. loop
+num = 5
+for i in range(num):
+	pass
+
+# 4. conditional statement
+a = 5
+b = 10
+if (a < b):
+	pass
+else:
+	print("b<=a")
+
+for letter in "Python3": 
+	if letter == "h":
+		pass
+	else:
+		print("", letter, end="")
+
+print("\ntest finish")
